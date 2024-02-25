fd70d1f2e4788cde20936fd115039b803917eb83
IrinaBelenko <akaiguren@gmail.com>
2024-02-25 22:23:11 +0200

    all animation

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -10,22 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*        val toolbar:Toolbar = findViewById(R.id.myToolbar)
-        toolbar.title = "My toolbar"
-        toolbar.inflateMenu(R.menu.my_menu)
-        toolbar.setOnMenuItemClickListener { item ->
-            when (item?.itemId) {
-                R.id.call -> {
-                    Toast.makeText(
-                        this@MainActivity, "Calling...",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                else -> Unit
-            }
-            true
-        }*/
-
     }
 }
